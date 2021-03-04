aaf28f4f8aa8809582b484d1b5af3854af180850
CBondee <64631148+CBondee@users.noreply.github.com>
2021-03-04 13:53:01 +0100

    Update RunAllSteps.ps1
    Hi I took the liberty to add parameters to the start. I noticed you used $startdir $startdir.
    /Claus

--- RunAllSteps.ps1
+++ RunAllSteps.ps1
@@ -1,8 +1,19 @@
+# this will allow the script to be executed from another location
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string] $startdir,
+    [Parameter(Mandatory = $true, Position = 1)]
+    [string] $configurationfile
+)
+
+
 # import the script with all the code
 . '.\Run Stryker.ps1'
 
 # save where we started
-$startDir = Get-Location
+# $startDir = Get-Location
 Write-Host "Starting at: " $startDir
 
-RunEverything $startDir $startDir
+RunEverything $startDir $configurationfile
